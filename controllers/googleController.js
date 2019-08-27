860c7c15e2981e5f46fcdfe7c555e8f5c2749122
arnel <arne.landwehr@innoq.com>
2019-08-27 14:50:23 +0200

    return correct response

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -2,8 +2,15 @@ const request = require('request');
 
 exports.reviewRatings = function getGoogleReviewRating(req, res) {
     request('https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyBTys33uJH7-oByYblJqun7EYOVldzMq5Y&placeid=ChIJ3_c1mbZ0sEcR98FkiQ6jCVo', (error, response, body) => {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body);
+        var content = JSON.parse(body);
+        if (!content.error_message) {
+            res.send({
+                count: content.result.user_ratings_total,
+                rating: content.result.rating,
+                url: content.result.url
+            });
+        } else {
+            res.send(body);
+        }
     });
 };
