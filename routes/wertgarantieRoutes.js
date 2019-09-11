5897ff2b061b3acbe95cc70fe83dca6420ea7338
arnel <arne.landwehr@innoq.com>
2019-09-11 10:36:09 +0200

    activate cors for all requests

--- routes/wertgarantieRoutes.js
+++ routes/wertgarantieRoutes.js
@@ -2,14 +2,8 @@ var express = require('express');
 var router = express.Router();
 var googleController = require("../controllers/googleController.js");
 var wertgarantieController = require("../controllers/wertgarantieController.js");
-var cors = require("cors");
 
-router.options("/rating", cors(), function (req, res, next) {
-    next();
-});
-
-router.get("/rating", cors(), googleController.reviewRatings);
-
-router.get("/policies", cors(), wertgarantieController.policies);
+router.get("/rating", googleController.reviewRatings);
+router.get("/policies", wertgarantieController.policies);
 
 module.exports = router;
