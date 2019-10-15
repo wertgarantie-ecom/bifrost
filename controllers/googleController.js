77775aba556057dbad57b74ffeebc657acf81a3c
henning <henning.nobbe@innoq.com>
2019-10-15 09:17:22 +0200

    Refactor google places call

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -1,16 +1,24 @@
 const request = require('request');
+const googleApiKey = process.env.GOOGLE_API_KEY;
 
 exports.reviewRatings = function getGoogleReviewRating(req, res) {
-    request('https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyBTys33uJH7-oByYblJqun7EYOVldzMq5Y&placeid=ChIJ3_c1mbZ0sEcR98FkiQ6jCVo', (error, response, body) => {
-        var content = JSON.parse(body);
-        if (!content.error_message) {
-            res.send({
-                text: content.result.user_ratings_total + " Google Reviews",
-                rating: content.result.rating,
-                uri: content.result.url
-            });
-        } else {
-            res.send(body);
-        }
-    });
+    if (!googleApiKey) {
+        console.log("Google API key not set");
+        res.send({
+            error: "Service currently not available!"
+        })
+    } else {
+        request('https://maps.googleapis.com/maps/api/place/details/json?key=' + googleApiKey + '&placeid=ChIJ3_c1mbZ0sEcR98FkiQ6jCVo', (error, response, body) => {
+            var content = JSON.parse(body);
+            if (!content.error_message) {
+                res.send({
+                    text: content.result.user_ratings_total + " Google Reviews",
+                    rating: content.result.rating,
+                    uri: content.result.url
+                });
+            } else {
+                res.send(body);
+            }
+        });
+    }
 };
