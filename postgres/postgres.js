9c4c06941f9b671069b0a2c818c7b05ab5570b46
henning <henning.nobbe@innoq.com>
2019-10-17 09:59:29 +0200

    switched name of environment var to DATABASE_URL

--- postgres/postgres.js
+++ postgres/postgres.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URI
+    connectionString: process.env.DATABASE_URL
 });
 
 function query(text, params, callback) {
