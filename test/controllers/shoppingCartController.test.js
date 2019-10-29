b1289ed1add0e8928517fc5ceb00641740c5433e
henning <henning.nobbe@innoq.com>
2019-10-29 16:03:03 +0100

    SWDBECOM-77 added shopping cart controller test to check if cookie is filled with product data

--- test/controllers/shoppingCartController.test.js
+++ test/controllers/shoppingCartController.test.js
@@ -2,12 +2,27 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 test('should return cookie with selected product', async () => {
-    const response = await request(app).post('/wertgarantie/shoppingCart/43446A56-3546-416D-B942-1262CA0526FB')
+    return request(app).post('/wertgarantie/shoppingCart/43446A56-3546-416D-B942-1262CA0526FB')
         .send({
             "productId": 12,
             "deviceClass": "17fd707a-f9c0-11e9-9694-cf549fcf64e2",
             "devicePrice": 45.0,
             "deviceCurrency": "EUR"
-        });
-    expect(response.statusCode).toBe(200);
+        })
+        .expect(200)
+        .expect('Set-Cookie', /clientId/)
+        .expect('Set-Cookie', /products/)
 });
+
+test('should fail when invalid request params are submitted', async () => {
+    return request(app).post('/wertgarantie/shoppingCart/43446A56-3546-416D-B942-1262CA0526FB')
+    .send({
+        "productId": 12,
+        "devicePrice": 45.0,
+        "deviceCurrency": "EUR"
+    })
+    .expect(400)
+    .expect(function(res) {
+        res.body.errors = "\"deviceClass\" is required"
+    })
+})
