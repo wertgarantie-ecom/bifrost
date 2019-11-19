a0356ec322f2b69aeb70a4d89d35aefa5dba0052
henning <henning.nobbe@innoq.com>
2019-11-19 13:26:26 +0100

    changed product images

--- src/services/policyService.js
+++ src/services/policyService.js
@@ -1,26 +1,31 @@
 const productImageMapping = {
     // Smartphone
     "1dfd4549-9bdc-4285-9047-e5088272dade": [
-        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/eBike-HP.jpeg",
-        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/utah-mountain-biking-bike-biking-71104.jpeg"
+        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/Basis.png",
+        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/Premium.png"
     ],
     // Handy
     "2dc16d28-c7af-4e19-9494-1659e1c27201": [
-        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/eBike-HP.jpeg",
-        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/utah-mountain-biking-bike-biking-71104.jpeg"
+        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/Basis.png",
+        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/Premium.png"
     ],
     // Surfbox
     "41ade4ba-5f24-4321-b706-fa6d05d75a73": [
-        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/eBike-HP.jpeg",
-        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/utah-mountain-biking-bike-biking-71104.jpeg"
+        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/Basis.png",
+        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/Premium.png"
     ],
     // Tablets
     "bb3a615d-e92f-4d24-a4cc-22f8a87fc544": [
-        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/eBike-HP.jpeg",
-        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/utah-mountain-biking-bike-biking-71104.jpeg"
+        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/Basis.png",
+        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/Premium.png"
     ],
     // Datenstick
     "ebfb2d44-4ff8-4579-9cc0-0a3ccb8d6f2d": [
+        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/Basis.png",
+        "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/Premium.png"
+    ],
+    // Datenstick
+    "bike": [
         "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/eBike-HP.jpeg",
         "https://wertgarantie-bifrost.s3.eu-central-1.amazonaws.com/utah-mountain-biking-bike-biking-71104.jpeg"
     ]
