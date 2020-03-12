39b73dd58134d5af9b56c30017dd3f98d3f72750
arnel <arne.landwehr@innoq.com>
2020-03-12 10:04:10 +0100

    SWDBECOM-188 always use correct jest reports env config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ require('iconv-lite/encodings');
 process.env.NODE_ENV = 'test';
 process.env.SIGN_SECRET = "irgendwas";
 process.env.HEIMDALL_URI = "http://heimdallDummyUrl";
+process.env.JEST_JUNIT_OUTPUT_DIR = "reports/junit/bifrost-unit-test-results.xml";
 
 module.exports = {
     testRegex: "/test/.*\.test\..*",
