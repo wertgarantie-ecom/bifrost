3c218bc3f5b9edd5a8b1fd1d102f42a4c0ee5cbc
henning <henning.nobbe@innoq.com>
2020-04-02 21:44:49 +0200

    SWDBECOM-226 local client data fixtures updated

--- fixtures/addClientData.fixture.js
+++ fixtures/addClientData.fixture.js
@@ -1,14 +1,72 @@
 const request = require('supertest');
 const app = require('../src/app');
 
-test('should add new client with valid data', async (done) => {
+test('should add phone test shop client configuration', async (done) => {
     const validData = {
-        name: "local-testShop",
+        name: "Test Shop Handy",
         secrets: [
-            "test-secret"
+            "test-phone-secret"
         ],
         publicClientIds: [
             "5209d6ea-1a6e-11ea-9f8d-778f0ad9137f"
+        ]
+    };
+
+    const response = await request(app)
+        .get(`/wertgarantie/clients?publicClientId=${validData.publicClientIds[0]}`)
+        .auth(process.env.BASIC_AUTH_USER, process.env.BASIC_AUTH_PASSWORD)
+        .set('Accept', 'application/json')
+        .send(validData);
+
+    console.log(JSON.stringify(response.body, null, 2));
+    if (response.status !== 200) {
+        await request(app)
+            .post("/wertgarantie/clients")
+            .auth(process.env.BASIC_AUTH_USER, process.env.BASIC_AUTH_PASSWORD)
+            .set('Accept', 'application/json')
+            .send(validData)
+            .expect(200);
+    }
+    done();
+});
+
+test('should add bike test shop client configuration', async (done) => {
+    const validData = {
+        name: "Test Shop Bike",
+        secrets: [
+            "test-bike-secret"
+        ],
+        publicClientIds: [
+            "5a576bd2-1953-4d20-80de-4de00d65fdc7"
+        ]
+    };
+
+    const response = await request(app)
+        .get(`/wertgarantie/clients?publicClientId=${validData.publicClientIds[0]}`)
+        .auth(process.env.BASIC_AUTH_USER, process.env.BASIC_AUTH_PASSWORD)
+        .set('Accept', 'application/json')
+        .send(validData);
+
+    console.log(JSON.stringify(response.body, null, 2));
+    if (response.status !== 200) {
+        await request(app)
+            .post("/wertgarantie/clients")
+            .auth(process.env.BASIC_AUTH_USER, process.env.BASIC_AUTH_PASSWORD)
+            .set('Accept', 'application/json')
+            .send(validData)
+            .expect(200);
+    }
+    done();
+});
+
+test('should add Handyflash DEV test client configuration', async (done) => {
+    const validData = {
+        name: "Handyflash DEV",
+        secrets: [
+            "test-handyflash-secret"
+        ],
+        publicClientIds: [
+            "b9f303d0-74e1-11ea-b9e9-034d1bd36e8d"
         ]
     };
 
