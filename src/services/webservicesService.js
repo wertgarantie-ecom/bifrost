2ef499cea76fe25a79e6038537460f4ed887a956
arnel <arne.landwehr@innoq.com>
2020-04-09 15:08:10 +0200

    SWDBECOM-233 add some totaly buggy stuff

--- src/services/webservicesService.js
+++ src/services/webservicesService.js
@@ -12,6 +12,7 @@ async function selectRelevantWertgarantieProducts(session, clientConfig, webserv
     const productData = await webservicesClient.getAgentData(session);
     return _.filter(productData.RESULT.PRODUCT_LIST.PRODUCT, product => clientConfig.productOffersConfigurations.reduce((acc, productOfferConfig) => acc || productOfferConfig.productType === product.PRODUCT_TYPE, false));
 }
+
 exports.selectRelevantWertgarantieProducts = selectRelevantWertgarantieProducts;
 
 async function assembleAllProductOffers(clientConfig, webservicesClient = _webservicesClient) {
@@ -34,33 +35,70 @@ async function assembleProductOffers(session, productOfferConfig, clientId, allW
         devices: await getDevicePremiums(session, productOfferConfig, webservicesProduct)
     };
 }
+
 exports.assembleProductOffers = assembleProductOffers;
 
 async function findProductFor(productOfferConfig, allWebservicesProductsForClient) {
     return _.find(allWebservicesProductsForClient, product => product.APPLICATION_CODE === productOfferConfig.applicationCode && product.PRODUCT_TYPE === productOfferConfig.productType);
 }
+
 exports.findProductFor = findProductFor;
 
 async function getDocuments(session, productOfferConfig) {
     const documents = [];
-    documents.push(...getLegalDocuments(session, productOfferConfig, ));
+    documents.push(...getLegalDocuments(session, productOfferConfig,));
     documents.push(...getComparisonDocuments());
     return documents;
 }
+
 exports.getDocuments = getDocuments;
 
-async function getDevicePremiums(session, productOfferConfig, webservicesProduct) {
-    getDevices().max(device => getPremiums())
+function findMaxPriceForDeviceClass(webservicesProduct, deviceClassConfig) {
+    const priceLimitation = _.find(webservicesProduct.PURCHASE_PRICE_LIMITATIONS.MAX_PRICE, limit => limit.OBJECT_CODE === deviceClassConfig.objectCode);
+    return priceLimitation ? priceLimitation.AMOUNT : undefined;
 }
+
+async function getDevicePremiums(session, productOfferConfig, webservicesProduct, webservicesClient = _webservicesClient) {
+    return productOfferConfig.deviceClasses.map(deviceClassConfig => {
+        const deviceClass = {
+            objectCode: deviceClassConfig.objectCode,
+            objectCodeExternal: deviceClassConfig.objectCodeExternal,
+            maxPriceLimitation: findMaxPriceForDeviceClass(webservicesProduct, deviceClassConfig),
+            intervals: []
+        };
+
+        const riskTypesToConsider = [clientConfig.basicRiskType].concat(productOfferConfig.risks);
+
+
+        const intervals = PAYMENTINTERVALS.INTERVAL.map(async interval => {
+            // ... pro Preisstaffelgrenze premiums ab
+            const intervalPremiums = {};
+            intervalPremiums[interval.VALUE] = await Promise.all(clientConfig.relevantProductTypes[product.PRODUCT_TYPE].priceRanges.map(async (range) => {
+                // setze Object Code = irgendein Code aus den Agent Data für dieses Versicherungsprodukt... Komplettschutz z. B. scheint sich ja für alle inkludierten Produkte gleich zu verhalten, oder? bis 300€ --> 5€, bis 800€ --> 8€ usw.
+                // ist das noch abhängig vom Object Code?
+                const result = await this.getInsurancePremium(session, product.APPLICATION_CODE, product.PRODUCT_TYPE, interval.VALUE, product.PURCHASE_PRICE_LIMITATIONS.MAX_PRICE[0].OBJECT_CODE, range.maxPrice, riskTypesToConsider, webservicesClient);
+                return {
+                    minClose: range.minClose,
+                    maxOpen: scale.maxOpen,
+                    insurancePrime: result.RESULT.PREMIUM_RECURRING
+                };
+            }));
+
+            deviceClass.intervals.push(...intervals);
+        })
+
+    });
+}
+
 exports.getDevicePremiums = getDevicePremiums;
 
 
-exports.assembleProductOffers = async function assembleProductOffers(clientConfig, webservicesClient = _webservicesClient) {
+async function assembleProductOffers(clientConfig, webservicesClient = _webservicesClient) {
     const session = await webservicesClient.login(clientConfig);
     const relevantProducts = selectRelevantWertgarantieProducts(clientConfig, webservicesClient);
 
 
-    const productOffers = await Promise.all(relevantProducts.map(async product => {
+    return await Promise.all(relevantProducts.map(async product => {
         const productOffer = {
             id: uuid(),
             clientId: clientConfig.publicClientIds[0],
@@ -69,73 +107,8 @@ exports.assembleProductOffers = async function assembleProductOffers(clientConfi
             deviceClasses: [],
             documents: []
         };
-        // get legal documents
-        const legalDocuments = await getLegalDocuments(session, product.APPLICATION_CODE, product.PRODUCT_TYPE, clientConfig, webservicesClient);
-        productOffer.documents = productOffer.documents.concat(legalDocuments);
-
-        // pro versicherbarem product wollen wir sowas hier haben:
-        /*
-          deviceClass:
-        * {
-                "objectCode": "9025",
-                "premiums": [
-                    {
-                        "Komplettschutz": {
-                            1: [
-                                {
-                                    "maxProductPrice": 300,
-                                    "insurancePrime": 5
-                                },
-                                {
-                                    "maxProductPrice": 800,
-                                    "insurancePrime": 8
-                                },
-                                {
-                                    "maxProductPRice": 1800,
-                                    "insurancePrime": 11
-                                }
-                            ]
-                      }
-                ]
-            }
-        * */
-        // Wir brauchen doch schon bei uns ein Mapping von Shop-Product-Name zu OBJECT_CODE... bei der Jobanfrage haben wir ja noch keinen Produkt-Typ über die Selection Component rein bekommen und müssen uns darauf beziehen, was in der agent data steht...
-        // Von daher hat das ganze Mapping bei den Webservices für uns eigentlich keinen Vorteil, da wir eh alle Anfragen aus unserem Cache bedienen werden...
-
-        // frage pro riskType zusammensetzung (productOffers - configuration unten in clientConfig) die Versicherungsprämien für einzelne Preisstufen und Zahlungsintervalle ab
-        const deviceClassPremiums = await Promise.all(clientConfig.productOfferConfigurations.map(async (productOfferConfig) => {
-            const productOfferPremiumsPerInterval = {};
-            const riskTypesToConsider = [clientConfig.basicRiskType].concat(productOfferConfig.risks);
-
-            productOfferPremiumsPerInterval[productOfferConfig.name] = {};
-
-            // frage pro payment interval...
-            const premiums = await Promise.all(product.PAYMENTINTERVALS.INTERVAL.map(async (interval) => {
-                // ... pro Preisstaffelgrenze premiums ab
-                const intervalPremiums = {};
-                intervalPremiums[interval.VALUE] = await Promise.all(clientConfig.relevantProductTypes[product.PRODUCT_TYPE].scalesOfPrices.map(async (maxPrice) => {
-                    // setze Object Code = irgendein Code aus den Agent Data für dieses Versicherungsprodukt... Komplettschutz z. B. scheint sich ja für alle inkludierten Produkte gleich zu verhalten, oder? bis 300€ --> 5€, bis 800€ --> 8€ usw.
-                    // ist das noch abhängig vom Object Code?
-                    const result = await this.getInsurancePremium(session, product.APPLICATION_CODE, product.PRODUCT_TYPE, interval.VALUE, product.PURCHASE_PRICE_LIMITATIONS.MAX_PRICE[0].OBJECT_CODE, maxPrice, riskTypesToConsider, webservicesClient);
-                    return {
-                        maxProductPrice: maxPrice,
-                        insurancePrime: result.RESULT.PREMIUM_RECURRING
-                    };
-                }));
-
-                return intervalPremiums;
-            }));
-            premiums.map(premium => {
-                const intervalValue = Object.keys(premium)[0];
-                productOfferPremiumsPerInterval[productOfferConfig.name][intervalValue] = premium[intervalValue];
-            });
-            return productOfferPremiumsPerInterval;
-        }));
-        productOffer.deviceClasses = productOffer.deviceClasses.concat(deviceClassPremiums);
-        return productOffer;
     }));
-    return productOffers;
-};
+}
 
 // for testing purposes
 exports.getInsurancePremium = async function getInsurancePremium(session, applicationCode, productType, intervalValue, objectCode, objectPrice, riskTypes, webservicesClient = _webservicesClient) {
@@ -157,6 +130,7 @@ async function getLegalDocuments(session, productOfferConfig, webservicesClient 
         };
     }));
 }
+
 exports.getLegalDocuments = getLegalDocuments;
 
 async function getComparisonDocuments(session, productOfferConfig, webservicesClient = _webservicesClient, documentRespository = _documentRespository) {
@@ -174,9 +148,11 @@ async function getComparisonDocuments(session, productOfferConfig, webservicesCl
         };
     }));
 }
+
+// pro Device Class  habe ich N Intervalle, pro Intervall habe ich X price scales = N * X Abfragen der Prämien pro Offer
 exports.getComparisonDocuments = getComparisonDocuments;
 
-const webservicesProductOffersConfig = {
+const sampleConfig = {
     clientIdHandyFlash: {
         productOffersConfigurations: [
             {
@@ -188,12 +164,36 @@ const webservicesProductOffersConfig = {
                     {
                         objectCode: 9025,
                         externalObjectCode: "Smartphone",
-                        scalesOfPrices: [300, 800, 1800]
+                        priceRanges: [
+                            {
+                                min: 0,
+                                max: 300
+                            },
+                            {
+                                min: 300,
+                                max: 800
+                            },
+                            {
+                                min: 800,
+                                max: 1800
+                            }]
                     },
                     {
                         objectCode: 73,
                         externalObjectCode: "Mobilfunk",
-                        scalesOfPrices: [300, 800, 1800]
+                        priceRanges: [
+                            {
+                                min: 0,
+                                max: 300
+                            },
+                            {
+                                min: 300,
+                                max: 800
+                            },
+                            {
+                                min: 800,
+                                max: 1800
+                            }]
                     }
 
                 ],
@@ -218,7 +218,22 @@ const webservicesProductOffersConfig = {
                     {
                         objectCode: 9025,
                         externalObjectCode: "Smartphone",
-                        scalesOfPrices: [300, 800, 1800]
+                        scalesOfPrices:
+                            [
+                                {
+                                    min: 0,
+                                    max: 300
+                                },
+                                {
+                                    min: 300,
+                                    max: 800
+                                },
+                                {
+                                    min: 800,
+                                    max: 1800
+                                }
+
+                            ]
                     },
                     {
                         objectCode: 73,
@@ -241,131 +256,6 @@ const webservicesProductOffersConfig = {
             }
         ]
     }
-    // ,
-    // clientIdBOC: {
-    //     productOfferConfigurations: [
-    //         {
-    //             interval: "monthly",
-    //             name: "Schutz mit monatlicher Bezahlweise"
-    //         },
-    //         {
-    //             interval: "yearly",
-    //             name: "Schutz mit jährlicher Bezahlweise"
-    //         }
-    //     ],
-    //     singlePaymentFeatures: {
-    //         risks: ["SOFORTSCHUTZ"]
-    //     },
-    //     recurringPaymentFeatures: {
-    //         risks: ["AKKUSCHUTZ"]
-    //     }
-    // }
 };
 
-const webservicesServiceProductOffers = [
-    {
-        "id": "uuid", // generated
-        "clientId": "clientId",
-        "productType": "KOMPLETTSCHUTZ_2019",
-        "applicationCode": "GU WG BLA BLA",
-        "riskTypes": [
-            "KOMPLETTSCHUTZ"
-        ],
-        "name": "KOMPLETTSCHUTZ", //aus client config für Produkt --> selbst pflegen,
-        "advantages": ["ADVERTISING_TEXT"], // + was auch immer wir in der client config mit pflegen
-        "deviceClasses": [
-            {
-                "objectCode": "9025",
-                "premiums": [
-                    {
-                        "Komplettschutz": {
-                            monthly: [
-                                {
-                                    "maxProductPrice": 300,
-                                    "insurancePrime": 5,
-                                    "singlePaymentOptions": [],
-                                },
-                                {
-                                    "maxProductPrice": 800,
-                                    "insurancePrime": 8,
-                                    "singlePaymentOptions": [],
-                                },
-                                {
-                                    "maxProductPRice": 1800,
-                                    "insurancePrime": 11,
-                                    "singlePaymentOptions": [],
-                                }
-                            ],
-                            quarterly: [
-                                {
-                                    "maxProductPrice": 300,
-                                    "insurancePrime": 15,
-                                    "singlePaymentOptions": [],
-                                },
-                                {
-                                    "maxProductPrice": 800,
-                                    "insurancePrime": 24,
-                                    "singlePaymentOptions": [],
-                                },
-                                {
-                                    "maxProductPRice": 1800,
-                                    "insurancePrime": 33,
-                                    "singlePaymentOptions": [],
-                                }
-                            ]
-                        },
-                        "Komplettschutz mit Premium-Option": {
-                            monthly: [
-                                {
-                                    "maxProductPrice": 300,
-                                    "insurancePrime": 6.95,
-                                    "singlePaymentOptions": [],
-                                },
-                                {
-                                    "maxProductPrice": 800,
-                                    "insurancePrime": 9.95,
-                                    "singlePaymentOptions": [],
-                                },
-                                {
-                                    "maxProductPRice": 1800,
-                                    "insurancePrime": 12.95,
-                                    "singlePaymentOptions": [],
-                                }
-                            ],
-                            quarterly: [
-                                {
-                                    "maxProductPrice": 300,
-                                    "insurancePrime": 20.85,
-                                    "singlePaymentOptions": [],
-                                },
-                                {
-                                    "maxProductPrice": 800,
-                                    "insurancePrime": 29.85,
-                                    "singlePaymentOptions": [],
-                                },
-                                {
-                                    "maxProductPRice": 1800,
-                                    "insurancePrime": 38.85,
-                                    "singlePaymentOptions": [],
-                                }
-                            ]
-                        }
-                    }
-                ],
-            }
-        ],
-        "documents": [ // COMPARISON_DOCUMENT + LEGAL_DOCUMENTS
-            {
-                "document_type": "",
-                "document_title": "",
-                "document_link": ""
-            },
-            {
-                "document_type": "",
-                "document_title": "",
-                "document_link": ""
-            }
-        ]
-    },
-];
 
