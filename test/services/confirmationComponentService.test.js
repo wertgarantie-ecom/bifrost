05df4b6b0c54995451367daae70ee96f12e46ab2
henning <henning.nobbe@innoq.com>
2020-04-22 01:05:27 +0200

    SWDBECOM-238 fix failing tests

--- test/services/confirmationComponentService.test.js
+++ test/services/confirmationComponentService.test.js
@@ -104,7 +104,7 @@ const expectedResponse = {
             orderId: "18ff0413-bcfd-48f8-b003-04b57762067a"
         },
     ],
-    generalConfirmationText: 'Ich akzeptiere die Allgemeinen Versicherungsbedingungen <a href="http://localhost:3000/documents/justnotthere">(AVB)</a> und die Bestimmungen zum Datenschutz. \n' +
+    generalConfirmationText: 'Ich akzeptiere die Allgemeinen Versicherungsbedingungen <a href="http://localhost:3000/documents/da39a3ee5e6b4b0d3255bfef95601890afd80709">(AVB)</a> und die Bestimmungen zum Datenschutz. \n' +
         '                                    Das gesetzliche Widerrufsrecht, die Produktinformationsblätter und die Vermittler-Erstinformation habe ich \n' +
         '                                    zur Kenntnis genommen und alle Dokumente heruntergeladen. Mit der Bestätigung der Checkbox erkläre ich mich damit \n' +
         '                                    einverstanden, dass mir alle vorstehenden Unterlagen an meine E-Mail-Adresse übermittelt werden. Der Übertragung \n' +
