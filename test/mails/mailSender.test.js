483342f4992af4a04405ef05694588d545b45a21
arnel <arne.landwehr@innoq.com>
2020-05-08 14:54:01 +0200

    SWDBECOM-269 update keys

--- test/mails/mailSender.test.js
+++ test/mails/mailSender.test.js
@@ -48,33 +48,3 @@ test('should send shop report mails without failures', async () => {
     expect(!!emailBody).toEqual(true);
 })
 
-test('should send shop report mails without failures', async () => {
-    let emailBody;
-    const testOptions = {
-        domain: "sandbox15ae90a13739495f99adaf40cf6bc26f.mailgun.org",
-        apiKey: "bf34e19f27faa9b34d783d0b203e254a-0afbfc6c-4e9c3b9d",
-        testMode: false,
-        testModeLogger: (httpOptions, payload) => {
-            emailBody = payload;
-        }
-    }
-
-    const purchase = {
-        id: uuid(),
-        orderId: uuid(),
-        wertgarantieProductId: uuid(),
-        wertgarantieProductName: "Komplettschutz Basis",
-        deviceClass: "Smartphone",
-        devicePrice: 120000,
-        success: true,
-        message: "successfully transmitted insurance proposal",
-        shopProduct: "IPhone X",
-        contractNumber: "45345464",
-        transactionNumber: uuid(),
-        backend: "webservices"
-    }
-
-    await mailSender.sendReportMailToShop('handyflash', 'arnelandwehr@gmail.com', purchase, validCustomer(), testOptions);
-
-    expect(!!emailBody).toEqual(true);
-})
