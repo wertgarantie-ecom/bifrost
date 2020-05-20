5c868731da6208881ad5339b9336d7409e084c4f
arnel <arne.landwehr@innoq.com>
2020-05-20 11:12:33 +0200

    SWDBECOM-285 fix new basic auth by clientid filter

--- src/routes/basicAuthByClientIdFilter.js
+++ src/routes/basicAuthByClientIdFilter.js
@@ -1,8 +1,16 @@
 const basicAuth = require('express-basic-auth');
 const clientRepository = require('../clientconfig/clientRepository');
+const isUUID = require('is-uuid');
 
 async function basicAuthByClientId(req, res, next) {
+    const clientId = req.params.clientId;
+    if (!isUUID.anyNonNil(clientId)) {
+        return res.sendStatus(400);
+    }
     const clientConfig = await clientRepository.findClientById(req.params.clientId);
+    if (!(clientConfig && clientConfig.basicAuthUser && clientConfig.basicAuthPassword)) {
+        return res.sendStatus(204);
+    }
     const options = {
         users: {},
         challenge: true
