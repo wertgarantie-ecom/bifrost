ed3c75679c4c10b7ea424fbdcf1bc6758979d261
arnel <arne.landwehr@innoq.com>
2020-05-26 14:10:44 +0200

    SWDBECOM-292 enable metrics

--- src/framework/metrics.js
+++ src/framework/metrics.js
@@ -3,12 +3,12 @@ var StatsD = require('hot-shots');
 let dogstasD;
 
 function metrics() {
-    if (process.env.METRICS_ENABLED) {
+    if (process.env.METRICS_ENABLED === 'true') {
         if (dogstasD) {
             return dogstasD;
         } else {
             dogstasD = new StatsD({
-                mock: process.env.METRICS_MOCK,
+                mock: process.env.METRICS_MOCK === 'true',
                 globalTags: {env: process.env.NODE_ENV},
             });
             return dogstasD;
