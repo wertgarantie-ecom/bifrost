685ff8c6e4a66de243692b3070c41833bd0316e1
arnel <arne.landwehr@innoq.com>
2020-06-05 17:38:51 +0200

    SWDBECOM-309 add handbook css

--- src/handbook/handbookService.js
+++ src/handbook/handbookService.js
@@ -197,6 +197,21 @@ Weitere Details zur Implementierung befinden sich [hier](https://wertgarantie-ec
     <title>Installation Instructions for ${client.name}</title>
 </head>
 <body>
+<style>
+	.markdown-body {
+		box-sizing: border-box;
+		min-width: 200px;
+		max-width: 980px;
+		margin: 0 auto;
+		padding: 45px;
+	}
+
+	@media (max-width: 767px) {
+		.markdown-body {
+			padding: 15px;
+		}
+	}
+</style>
     <div class="markdown-body">
         ${installationInstructionsMarkdown}
     </div>
