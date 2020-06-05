f2fa6c00d53a0aa38463c58bf75bf08d808d69fd
arnel <arne.landwehr@innoq.com>
2020-06-05 14:15:27 +0200

    SWDBECOM-309 fix auth check filter

--- src/routes/basicAuthByClientIdFilter.js
+++ src/routes/basicAuthByClientIdFilter.js
@@ -1,22 +1,22 @@
-const basicAuth = require('express-basic-auth');
-const clientRepository = require('../clientconfig/clientRepository');
+const _basicAuth = require('express-basic-auth');
+const _clientRepository = require('../clientconfig/clientRepository');
 const isUUID = require('is-uuid');
 
-async function basicAuthByClientId(req, res, next) {
+async function basicAuthByClientId(req, res, next, clientRepository = _clientRepository, basicAuth = _basicAuth, env = process.env) {
     const clientId = req.params.clientId;
     if (!isUUID.anyNonNil(clientId)) {
         return res.sendStatus(400);
-    }
-    const clientConfig = await clientRepository.findClientById(req.params.clientId);
-    if (!(clientConfig && clientConfig.basicAuthUser && clientConfig.basicAuthPassword)) {
-        return res.sendStatus(204);
     }
     const options = {
         users: {},
         challenge: true
     };
-    options.users[clientConfig.basicAuthUser] = clientConfig.basicAuthPassword;
-    options.users[process.env.BASIC_AUTH_USER] = process.env.BASIC_AUTH_PASSWORD;
+
+    const clientConfig = await clientRepository.findClientById(req.params.clientId);
+    if (clientConfig && clientConfig.basicAuthUser && clientConfig.basicAuthPassword) {
+        options.users[clientConfig.basicAuthUser] = clientConfig.basicAuthPassword;
+    }
+    options.users[env.BASIC_AUTH_USER] = env.BASIC_AUTH_PASSWORD;
     return basicAuth(options)(req, res, next);
 }
 
---
+++ test/routes/basicAuthByClientIdFilter.test.js
@@ -0,0 +1,112 @@
+const filter = require('../../src/routes/basicAuthByClientIdFilter');
+
+test('should accept request if correct client credentials are provided', async () => {
+    const req = {
+        params: {
+            clientId: "8915c640-d9f7-4236-adea-764393b2dcfd"
+        }
+    }
+    const res = {};
+    const next = _ => _;
+    const clientRepositoryMock = {
+        findClientById: () => {
+            return {
+                basicAuthUser: 'clientUser',
+                basicAuthPassword: 'clientPassword'
+            }
+        }
+    }
+    let givenOptions;
+    const basicAuthMock = (options) => {
+        givenOptions = options;
+        return _ => _;
+    };
+
+    await filter(req, res, next, clientRepositoryMock, basicAuthMock);
+
+    expect(givenOptions).toEqual({
+        challenge: true,
+        users: {
+            clientUser: "clientPassword"
+        }
+    });
+});
+
+test('should accept request if only admin credential are provided', async () => {
+    const req = {
+        params: {
+            clientId: "8915c640-d9f7-4236-adea-764393b2dcfd"
+        }
+    }
+    const res = {
+        sendStatus: () => {
+            throw new Error("should never call me!")
+        }
+    };
+    const next = _ => _;
+    const clientRepositoryMock = {
+        findClientById: () => {
+            return {}
+        }
+    }
+    let givenOptions;
+    const basicAuthMock = (options) => {
+        givenOptions = options;
+        return _ => _;
+    };
+    const env = {
+        BASIC_AUTH_USER: 'adminUser',
+        BASIC_AUTH_PASSWORD: 'adminPassword'
+    }
+
+    await filter(req, res, next, clientRepositoryMock, basicAuthMock, env);
+
+    expect(givenOptions).toEqual({
+        challenge: true,
+        users: {
+            adminUser: "adminPassword"
+        }
+    });
+});
+
+test('should include client and admin credentials', async () => {
+
+    const req = {
+        params: {
+            clientId: "8915c640-d9f7-4236-adea-764393b2dcfd"
+        }
+    }
+    const res = {
+        sendStatus: () => {
+            throw new Error("should never call me!")
+        }
+    };
+    const next = _ => _;
+    const clientRepositoryMock = {
+        findClientById: () => {
+            return {
+                basicAuthUser: 'clientUser',
+                basicAuthPassword: 'clientPassword'
+            }
+        }
+    }
+    let givenOptions;
+    const basicAuthMock = (options) => {
+        givenOptions = options;
+        return _ => _;
+    };
+    const env = {
+        BASIC_AUTH_USER: 'adminUser',
+        BASIC_AUTH_PASSWORD: 'adminPassword'
+    }
+
+    await filter(req, res, next, clientRepositoryMock, basicAuthMock, env);
+
+    expect(givenOptions).toEqual({
+        challenge: true,
+        users: {
+            adminUser: "adminPassword",
+            clientUser: "clientPassword"
+        }
+    });
+})
