156282af22ae4a37cf0a6c788ae15edd9bc4f6f7
henning <henning.nobbe@innoq.com>
2020-08-04 14:05:52 +0200

    SWDBECOM-363 return 204 if no loader config is set for the client

--- src/components/loader/loaderConfigController.js
+++ src/components/loader/loaderConfigController.js
@@ -1,5 +1,5 @@
 exports.getLoaderConfig = async function getLoaderConfig(req, res, next) {
-    if (!req.clientConfig) {
+    if (!req.clientConfig || !req.clientConfig.loaderConfig) {
         return res.sendStatus(204);
     }
     return res.status(200).send(req.clientConfig.loaderConfig);
